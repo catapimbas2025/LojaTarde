9dca9b48de1f003b80cb20a1e7d2b1c88e0217b3
Pedro Henrique <pedrogringo2007@gmail.com>
2025-03-20 16:33:45 -0300

    Add project files.

---
+++ DAL/ClientesDAL.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Modelos;
+
+namespace DAL 
+{                                                                                                                                                                                                                                                                                                                                                                          
+    public class ClientesDAL
+    {
+       
+        public void Incluir(ClientesInformation cliente)
+        {
+            //CONEXÃO
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+              
+                cn.ConnectionString = "Data Source=DESKTOP-V4V6Q6M;Initial Catalog=LOJADB;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "Insere_Clientes";
+
+                //PARAMETROS DA STORED PROCEDURE
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pcodigo);
+
+                SqlParameter pnome = new SqlParameter("@nome", SqlDbType.VarChar, 100);
+                pnome.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pnome);
+
+                SqlParameter pemail = new SqlParameter("@email", SqlDbType.VarChar, 100);
+                pemail.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pemail);
+
+                SqlParameter ptelefone = new SqlParameter("@telefone", SqlDbType.VarChar, 80);
+                ptelefone.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(ptelefone);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+            }   
+
+            catch (Exception ex)
+            {
+
+                throw new Exception("Servidor SQL erro: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+
+
+        }
+        
+
+
+
+        public void Alterar(ClientesInformation cliente)
+        {
+            //CONEXÃO 
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+             cn.ConnectionString = "Data Source=DESKTOP-V4V6Q6M;Initial Catalog=LOJADB;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "altera_clientes";
+
+                //PARAMETROS DA STORED PROCEDURE
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pcodigo);
+
+                SqlParameter pnome = new SqlParameter("@nome", SqlDbType.VarChar, 100);
+                pnome.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pnome);
+
+                SqlParameter pemail = new SqlParameter("@email", SqlDbType.VarChar, 100);
+                pemail.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pemail);
+
+                SqlParameter ptelefone = new SqlParameter("@telefone", SqlDbType.VarChar, 80);
+                ptelefone.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(ptelefone);
+
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+                cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Servidor SQL erro: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+
+
+        public void Excluir(int codigo)
+        {
+            //CONEXÃO
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "Data Source=DESKTOP-V4V6Q6M;Initial Catalog=LOJADB;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "exclui_clientes";
+
+                //PARAMETROS DO CÓDIGO
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Value = codigo;
+                cmd.Parameters.Add(pcodigo);
+
+                cn.Open();
+                int resultado = cmd.ExecuteNonQuery();
+                if(resultado !=1)
+                {
+                    throw new Exception("Não foi possível excluir o cliente" + codigo);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Servidor SQL erro: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ LojaTarde/VendasInformation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class VendasInformation
+    {
+        private int _codigo;
+        public int Codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+         
+        private DateTime _data;
+        public DateTime Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        private int _quantidade;
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+
+        private byte _faturado;
+        public byte Faturado
+        {
+            get { return _faturado; }
+            set { _faturado = value; }
+        }
+
+        private int _codigoCliente;
+        public int CodigoCliente
+        {
+            get { return _codigoCliente; }
+            set { _codigoCliente = value; }
+        }
+
+        private int _codigoProduto;
+        public int CodigoProduto
+        {
+            get { return _codigoProduto; }
+            set { _codigoProduto = value; }
+        }
+    }
+}
